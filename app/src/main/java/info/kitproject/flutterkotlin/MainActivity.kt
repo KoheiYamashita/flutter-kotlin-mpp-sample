cd4c51161422b3fce0099d3a74bb19ff9c7dfd1d
KoheiYamashita <k-yamashita@kitproject.info>
2018-11-10 19:06:34 +0900

    Use flutter lib

--- app/src/main/java/info/kitproject/flutterkotlin/MainActivity.kt
+++ app/src/main/java/info/kitproject/flutterkotlin/MainActivity.kt
@@ -1,12 +1,17 @@
 package info.kitproject.flutterkotlin
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.FrameLayout
+import io.flutter.facade.Flutter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val flutterView = Flutter.createView(this, lifecycle, "main")
+        val layout = FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT)
+        setContentView(flutterView, layout)
     }
 }
