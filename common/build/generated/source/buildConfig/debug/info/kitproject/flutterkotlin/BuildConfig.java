daef9a7b4468e8203079bc40352cdcdba9148cf4
Neo <c.min.tseng@gmail.com>
2021-11-11 14:35:32 +0800

    upgrade gradle & agp & kotlin version

---
+++ common/build/generated/source/buildConfig/debug/info/kitproject/flutterkotlin/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package info.kitproject.flutterkotlin;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "info.kitproject.flutterkotlin";
+  public static final String BUILD_TYPE = "debug";
+}
